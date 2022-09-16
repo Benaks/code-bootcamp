42b123f1f30db2544efbd85f83df5981aeb3f5b1
Ben Akaager <103255013+Benaks@users.noreply.github.com>
2022-09-17 00:48:28 +0100

    Add files via upload

---
+++ Coding bootcamp/code.js
@@ -0,0 +1,14 @@
+function nextImg(){
+    document.getElementById('image').src = ('image-john.jpg');
+    document.getElementById('p').innerHTML = (' “ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer ” ')
+    document.getElementById('prof').innerHTML = ('Junior Front-End Developer')
+    document.getElementById('name').innerHTML = ('John Tarkpor')
+    
+    }
+function prevImg(){
+    document.getElementById('image').src = ('image-tanya.jpg');
+    document.getElementById('p').innerHTML = ('I’ve been interested in coding for a while but never taken the jump, until now.  I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”')
+    document.getElementById('prof').innerHTML = ('UX Engineer')
+    document.getElementById('name').innerHTML = ('Tanya Sinclair')
+
+}
